5e19ed503cc9a27282840257f11918a53defcc07
Michael Gaare <michael.gaare@gmail.com>
2018-11-27 17:24:51 -0500

    bump clojure dependency to 1.9 release with provided scope

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject org.purefn/sentenza "0.1.9-SNAPSHOT"
   :description "Data pipeline for building email records"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [clj-time "0.14.2"]
                  [me.raynes/fs "1.4.6"]
                  [org.clojure/core.async "0.3.443"]
                  [com.taoensso/timbre "4.10.0"]
-                 [cheshire "5.8.0"] ]
+                 [cheshire "5.8.0"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [com.stuartsierra/component "0.3.2"]
                                   [criterium "0.4.4"]]
