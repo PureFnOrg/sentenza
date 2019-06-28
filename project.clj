dd72667bcf08018fb4a5eae0d91eb3eac285b3f4
Matt Kocubinski <mkocubinski@gmail.com>
2019-06-28 10:55:53 -0400

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/sentenza "0.2.0-SNAPSHOT"
+(defproject org.purefn/sentenza "0.2.0"
   :description "Data pipeline for building email records"
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
                  [clj-time "0.14.2"]
